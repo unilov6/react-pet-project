738d86ed34b4a3610bf6c82795670a00a4dc08c0
unilov6 <danilunilov6@gmail.com>
2025-04-13 22:00:51 +0300

    first state and controlled component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [likes, setLikes] = useState(0)
+  const [value, setValue] = useState('input text')
+
+  const increment = () => {
+    setLikes(likes + 1)
+  }
+
+  const decrement = () => {
+    setLikes(likes - 1)
+  }
+
   return (
     <div className="App">
+      <h1>{likes}</h1>
+      <button onClick={increment}>Increase</button>
+      <button onClick={decrement}>Decrease</button>
+
+      <h1>{value}</h1>
+      <input 
+        type="text" 
+        value={value} 
+        onChange={event => setValue(event.target.value)}
+      />
     </div>
   );
 }
