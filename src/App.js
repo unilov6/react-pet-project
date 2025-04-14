10ae0d22030ae02d698fd3c971a45c1a80e044db
unilov6 <danilunilov6@gmail.com>
2025-04-14 16:31:00 +0300

    post list component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/App.css"
+import PostList from "./components/PostList";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaScript', body: 'Description'},
+    {id: 2, title: 'JavaScript 2', body: 'Description'},
+    {id: 3, title: 'JavaScript 3', body: 'Description'},
+  ])
   return (
     <div className="App">
-      <div className="post">
-        <div className="post__content">
-          <strong>1. JavaScript</strong>
-          <div>
-            JavaScript - is a programming language
-          </div>
-        </div>
-        <div className="post__btns">
-          <button>Delete</button>
-        </div>
-      </div>
+      <PostList posts={posts} title="Post list" />
     </div>
   );
 }
