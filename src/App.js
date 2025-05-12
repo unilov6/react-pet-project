abe0ad76258ff489dd7b0631620a8dfee978659a
unilov6 <danilunilov6@gmail.com>
2025-05-12 18:26:30 +0300

    first state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const increase = () => {
+    setCount(count + 1)
+  }
+
+  const decrease = () => {
+    setCount(count - 1)
+  }
+
   return (
     <div className="App">
-      
+        <h1>{count}</h1>
+      <button onClick={increase}>Increment</button>
+      <button onClick={decrease}>Decrement</button>
     </div>
   );
 }
