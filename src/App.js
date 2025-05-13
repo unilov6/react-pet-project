f8d180110815af626364b1db83638d8f60c25a02
unilov6 <danilunilov6@gmail.com>
2025-05-13 21:09:49 +0300

    posts form and creating posts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./styles/App.css"
 import PostList from "./components/PostList";
+import MyButton from "./UI/button/MyButton";
+import MyInput from "./UI/input/MyInput";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -8,8 +10,39 @@ function App() {
     {id: 2, title: 'JavaScript 2', body: 'Description'},
     {id: 3, title: 'JavaScript 3', body: 'Description'}
   ])
+
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+    const newPost = {
+      id: Date.now(),
+      title,
+      body
+    }
+    setPosts([...posts, newPost])
+    setTitle('')
+    setBody('')
+  }
+
   return (
     <div className="App">
+      <form>
+        <MyInput 
+          type="text" 
+          placeholder="Post name"
+          value={title}
+          onChange={e => setTitle(e.target.value)} 
+        />
+        <MyInput 
+          type="text" 
+          placeholder="Post description"
+          value={body}
+          onChange={e => setBody(e.target.value)} 
+        />
+        <MyButton onClick={addNewPost}>Create post</MyButton>
+      </form>
       <PostList posts={posts} title='Posts list' />
     </div>
   );
