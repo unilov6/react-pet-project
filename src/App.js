441a7df8dbae853035036eabfcfeab8ee641b7c8
unilov6 <danilunilov6@gmail.com>
2025-05-13 18:29:58 +0300

    first props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
-import Counter from "./components/Counter";
+import React, { useState } from "react";
+import "./styles/App.css"
+import PostItem from "./components/PostItem";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaScript', body: 'Description'},
+    {id: 2, title: 'JavaScript 2', body: 'Description'},
+    {id: 3, title: 'JavaScript 3', body: 'Description'}
+  ])
   return (
     <div className="App">
-        <Counter />
-        <Counter />
-        <Counter />
+      {posts.map(post => 
+        <PostItem post={post} key={post.id} />
+      )}
     </div>
   );
 }
