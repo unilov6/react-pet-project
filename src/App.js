e64f1189bd4725eb90fc4d6d27c3079e59235c3f
unilov6 <danilunilov6@gmail.com>
2025-05-14 10:30:52 +0300

    post form component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import "./styles/App.css"
 import PostList from "./components/PostList";
-import MyButton from "./UI/button/MyButton";
-import MyInput from "./UI/input/MyInput";
+import PostForm from "./components/PostForm";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -11,32 +10,13 @@ function App() {
     {id: 3, title: 'JavaScript 3', body: 'Description'}
   ])
 
-  const [post, setPost] = useState({title: '', body: ''})
-
-  const addNewPost = (e) => {
-    e.preventDefault()
-    
-    setPosts([...posts, {...post, id: Date.now()}])
-    setPost({title: '', body: ''})
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
   }
 
   return (
     <div className="App">
-      <form>
-        <MyInput 
-          type="text" 
-          placeholder="Post name"
-          value={post.title}
-          onChange={e => setPost({...post, title: e.target.value})}
-        />
-        <MyInput 
-          type="text" 
-          placeholder="Post description"
-          value={post.body}
-          onChange={e => setPost({...post, body: e.target.value})}
-        />
-        <MyButton onClick={addNewPost}>Create post</MyButton>
-      </form>
+      <PostForm create={createPost} />
       <PostList posts={posts} title='Posts list' />
     </div>
   );
