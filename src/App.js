cd61bbc30a4705f6be857145599c240777662338
unilov6 <danilunilov6@gmail.com>
2025-05-14 10:21:43 +0300

    optimized posts form

--- src/App.js
+++ src/App.js
@@ -11,19 +11,13 @@ function App() {
     {id: 3, title: 'JavaScript 3', body: 'Description'}
   ])
 
-  const [title, setTitle] = useState('')
-  const [body, setBody] = useState('')
+  const [post, setPost] = useState({title: '', body: ''})
 
   const addNewPost = (e) => {
     e.preventDefault()
-    const newPost = {
-      id: Date.now(),
-      title,
-      body
-    }
-    setPosts([...posts, newPost])
-    setTitle('')
-    setBody('')
+    
+    setPosts([...posts, {...post, id: Date.now()}])
+    setPost({title: '', body: ''})
   }
 
   return (
@@ -32,14 +26,14 @@ function App() {
         <MyInput 
           type="text" 
           placeholder="Post name"
-          value={title}
-          onChange={e => setTitle(e.target.value)} 
+          value={post.title}
+          onChange={e => setPost({...post, title: e.target.value})}
         />
         <MyInput 
           type="text" 
           placeholder="Post description"
-          value={body}
-          onChange={e => setBody(e.target.value)} 
+          value={post.body}
+          onChange={e => setPost({...post, body: e.target.value})}
         />
         <MyButton onClick={addNewPost}>Create post</MyButton>
       </form>
